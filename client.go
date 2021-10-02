79777146bc0ca95b5f4df54dd22583c415214129
sakura-rip <root@sakura.rip>
2021-10-02 20:14:02 +0900

    add: line client

---
+++ client.go
@@ -0,0 +1,38 @@
+package line
+
+import (
+	"github.com/line-api/model/go/model"
+	"golang.org/x/xerrors"
+)
+
+// ClientSetting line client setting
+type ClientSetting struct {
+	AppType model.ApplicationType
+	Proxy   string
+}
+
+// Client line client
+type Client struct {
+	ClientSetting *ClientSetting
+}
+
+// create default line client
+func newDefaultClient() *Client {
+	return &Client{
+		ClientSetting: &ClientSetting{
+			AppType: model.ApplicationType_ANDROID,
+		},
+	}
+}
+
+// New create new line client
+func New(opts ...ClientOption) (*Client, error) {
+	cl := newDefaultClient()
+	for idx, opt := range opts {
+		err := opt(cl)
+		if err != nil {
+			return nil, xerrors.Errorf("failed to execute %v option: %w", idx, err)
+		}
+	}
+	return cl, nil
+}
