dbd3faf2ec2fddadaebc21804e4ef82ca9929307
sakura-rip <root@sakura.rip>
2021-10-04 00:32:12 +0900

    update: login methods

--- login.go
+++ login.go
@@ -13,11 +13,24 @@ func (cl *Client) afterLogin() error {
 	return nil
 }
 
+func (cl *Client) LoginViaAuthKey(key string) error {
+	return cl.LoginViaPrimaryToken(cl.GeneratePrimaryToken(key))
+}
+
 func (cl *Client) LoginViaKeeper(mid string) error {
 	cl.Profile.Mid = mid
 	return cl.afterLogin()
 }
 
 func (cl *Client) LoginViaPrimaryToken(token string) error {
-	return nil
+	cl.TokenManager.IsV3Token = false
+	cl.TokenManager.AccessToken = token
+	return cl.afterLogin()
+}
+
+func (cl *Client) LoginViaV3Token(accessToken, refreshToken string) error {
+	cl.TokenManager.AccessToken = accessToken
+	cl.TokenManager.RefreshToken = refreshToken
+	cl.TokenManager.IsV3Token = true
+	return cl.afterLogin()
 }
