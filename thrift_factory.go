9c564e982edc4c06215a7755d5022bbefcc465de
sakura-rip <root@sakura.rip>
2021-10-04 10:41:13 +0900

    add: default http client

--- thrift_factory.go
+++ thrift_factory.go
@@ -2,15 +2,58 @@ package line
 
 import (
 	"github.com/line-api/model/go/model"
+	"net"
 	"net/http"
+	"net/url"
 )
 
 type thriftFactory struct {
-	client *Client
+	client        *Client
+	httpClient    *http.Client
+	defaultHeader map[string]string
 }
 
 func newThriftFactory(cl *Client) *thriftFactory {
-	return &thriftFactory{client: cl}
+	return &thriftFactory{
+		client:     cl,
+		httpClient: cl.defaultHttpClient(),
+		defaultHeader: map[string]string{
+			"x-line-application": cl.getLineApplicationHeader(),
+			"x-line-access":      cl.TokenManager.AccessToken,
+			"user-agent":         cl.getLineUserAgentHeader(),
+			"x-lal":              "ja_JP",
+			"x-lpv":              "1",
+		},
+	}
+}
+
+func (cl *Client) defaultHttpClient() *http.Client {
+	httpClient := &http.Client{Transport: &http.Transport{
+		ForceAttemptHTTP2:   true,
+		MaxIdleConns:        600,
+		MaxIdleConnsPerHost: 200,
+	}}
+	if cl.ClientSetting.LocalAddr != "" {
+		OKAddress, err := net.ResolveTCPAddr("tcp", cl.ClientSetting.LocalAddr)
+		if err != nil {
+			return httpClient
+		}
+		httpClient.Transport.(*http.Transport).DialContext = (&net.Dialer{
+			LocalAddr: OKAddress,
+		}).DialContext
+	}
+	if cl.ClientSetting.Proxy != "" {
+		httpClient.Transport.(*http.Transport).Proxy = parseProxyUrl(cl.ClientSetting.Proxy)
+	}
+	return httpClient
+}
+
+func parseProxyUrl(proxy string) func(*http.Request) (*url.URL, error) {
+	proxyUrl, err := url.Parse(proxy)
+	if err != nil {
+		return nil
+	}
+	return http.ProxyURL(proxyUrl)
 }
 
 func (f *thriftFactory) header() map[string]string {
