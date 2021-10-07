d5d9c89b9abaf6595811b91ae8e9ee65e6fc8eb6
sakura-rip <root@sakura.rip>
2021-10-07 19:32:41 +0900

    update: e2ee key store

--- crypt/e2ee_model.go
+++ crypt/e2ee_model.go
@@ -1,6 +1,8 @@
 package crypt
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type E2EESpecVersion int
 
@@ -11,23 +13,31 @@ const (
 )
 
 type E2EEKeyPair struct {
-	Owner string
-
 	KeyId int32
 
 	PrivateKeyId int32
 	PrivateKey   []byte `json:"private_key"`
 	PublicKeyId  int32
 	PublicKey    []byte `json:"public_key"`
+	Version      E2EESpecVersion
 }
 
-type E2EEKeyManagerIF interface {
-	Get(mid string, keyId int32) (*E2EEKeyPair, bool)
-	Set(mid string, keyId int32, keyPair *E2EEKeyPair)
+type E2EEStatus struct {
+	SpecVersion E2EESpecVersion
+	KeyId       int32
 }
 
 type E2EEKeyStore struct {
 	Data map[string]*E2EEKeyPair
+
+	Status map[string]*E2EEStatus
+}
+
+func NewE2EEKeyStore() *E2EEKeyStore {
+	return &E2EEKeyStore{
+		Data:   map[string]*E2EEKeyPair{},
+		Status: map[string]*E2EEStatus{},
+	}
 }
 
 func (s *E2EEKeyStore) formatKey(keyId int32, mid string) string {
@@ -36,8 +46,27 @@ func (s *E2EEKeyStore) formatKey(keyId int32, mid string) string {
 
 func (s *E2EEKeyStore) Get(mid string, keyId int32) (*E2EEKeyPair, bool) {
 	key, ok := s.Data[s.formatKey(keyId, mid)]
+	if ok {
+		s.Status[mid] = &E2EEStatus{
+			SpecVersion: key.Version,
+			KeyId:       keyId,
+		}
+	}
 	return key, ok
 }
+
+func (s *E2EEKeyStore) GetByMid(mid string) (*E2EEKeyPair, bool) {
+	status, ok := s.Status[mid]
+	if ok {
+		return s.Get(mid, status.KeyId)
+	}
+	return nil, false
+}
+
 func (s *E2EEKeyStore) Set(mid string, keyId int32, key *E2EEKeyPair) {
 	s.Data[s.formatKey(keyId, mid)] = key
+	s.Status[mid] = &E2EEStatus{
+		SpecVersion: key.Version,
+		KeyId:       keyId,
+	}
 }
